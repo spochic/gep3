5e7eab7559acf2349612dd3627a7ea986b43af22
Sebastien Pochic <spochic@ellipse.la>
2022-12-22 15:52:43 -0800

    emv.commands: support CommandApdu

--- src/emv/commands.py
+++ src/emv/commands.py
@@ -2,36 +2,85 @@
 """
 
 # Standard library imports
+from enum import Enum
+from typing import Union
 
 # Third party imports
 
 # Local application imports
-from common.ber import encode_length as length
 from common.ber import encode
+from common.iso7816 import CommandApdu, CommandField
+from common.hstr import clean as _clean
 
 
-def SELECT(aid: str) -> dict:
-    """SELECT(): generate APDU for SELECT command
+# Definitions
+
+
+class ApplicationIdentifier(Enum):
+    Mastercard = 'A0000000041010'
+    Visa = 'A0000000031010'
+    Maestro = 'A00000000410103060'
+    EVC = 'A0000008864040'
+    EVC_test = 'A0000008863030'
+    Dummy = 'A0000008862020'
+    Dummy_test = 'A0000008861010'
+
+
+class GetDataObject(Enum):
+    ATC = '9F36'
+    LastOnlineATCRegister = '9F13'
+    PINTryCounter = '9F17'
+    LogFormat = '9F4f'
+
+
+def SELECT(aid: Union[str, ApplicationIdentifier]) -> CommandApdu:
+    """SELECT(): generate CommandApdu for SELECT command
     """
-    known_aid_list = {'MASTERCARD' : 'A0000000041010', 'MAESTRO' : 'A00000000410103060', 'VISA' : 'A0000000031010'}
-    return {'header' : '00A40400', 'data' : known_aid_list.get(aid, aid), 'Le' : '00'}
+    apdu = {CommandField.Header: '00A40400', CommandField.Le: '00'}
 
-def GET_PROCESSING_OPTIONS(pdol: str = '') -> dict:
+    if aid in ApplicationIdentifier:
+        apdu[CommandField.Data] = aid.value
+    else:
+        apdu[CommandField.Data] = _clean(aid, "emv.SELECT()", 'aid')
+
+    return CommandApdu.from_dict(apdu)
+
+
+def GET_PROCESSING_OPTIONS(pdol: str = '') -> CommandApdu:
     """GET_PROCESSING_OPTIONS(): generate APDU for GET PROCESSING OPTIONS command
     """
-    return {'header' : '80A80000', 'data' : encode('83', pdol), 'Le' : '00'}
+    apdu = {CommandField.Header: '80A80000',
+            CommandField.Data: encode('83', pdol),
+            CommandField.Le: '00'}
 
-def GPO(pdol: str) -> dict:
+    return CommandApdu.from_dict(apdu)
+
+
+def GPO(pdol: str) -> CommandApdu:
     """GPO(): generate APDU for GET PROCESSING OPTIONS command
     """
     return GET_PROCESSING_OPTIONS(pdol)
 
-def READ_RECORD(SFI:int, record:int) -> dict:
+
+def READ_RECORD(SFI: int, record: int) -> CommandApdu:
     """READ_RECORD(): generate APDU for READ RECORD command
     """
-    return {'CLA' : '00', 'INS' : 'B2', 'P1' : F"{record:02X}", 'P2' : F"{SFI*8+4:02X}", 'Le' : '00'}
+    apdu = {CommandField.Class: '00',
+            CommandField.Instruction: 'B2',
+            CommandField.P1: F"{record:02X}",
+            CommandField.P2: F"{SFI*8+4:02X}",
+            CommandField.Le: '00'}
 
-def GET_DATA(tag: str) -> dict:
+    return CommandApdu.from_dict(apdu)
+
+
+def GET_DATA(tag: str) -> CommandApdu:
     """GET_DATA: generate APDU for GET DATA command
     """
-    return {'header' : '80CA' + tag, 'Le' : '00'}
+    apdu = {CommandField.Class: '80',
+            CommandField.Instruction: 'CA',
+            CommandField.P1: tag[0:2],
+            CommandField.P2: tag[2:4],
+            CommandField.Le: '00'}
+
+    return CommandApdu.from_dict(apdu)
