9d35842b583d17c43e52a093e33ad716fc54acf7
Sebastien Pochic <spochic@ellipse.la>
2022-12-23 13:59:24 -0800

    iso7816.commands: SELECT() with Enums

--- src/iso7816/commands.py
+++ src/iso7816/commands.py
@@ -2,32 +2,60 @@
 """
 
 # Standard library imports
-from enum import Enum
+from enum import IntEnum
 
 # Third party imports
 
 # Local application imports
-from .apdu import CommandField, CommandApdu
+from common.hstr import clean as _clean
+from iso7816.apdu import CommandField, CommandApdu
+from iso7816.encodings import SecureMessaging, Chaining, CLA
 
-# Definitions
+# Enum Definitions
 
 
+class Selection(IntEnum):
+    SelectMForDForEF = 0x00
+    SelectChildDF = 0x01
+    SelectEFUnderCurrentDF = 0x02
+    SelectParentDFOfCurrentDF = 0x03
+    SelectByDFName = 0x04
+    SelectFromMF = 0x08
+    SelectFromCurrentDF = 0x09
+
+
+class FileOccurance(IntEnum):
+    FirstOrOnlyOccurrence = 0x0
+    LastOccurrence = 0x1
+    NextOccurrence = 0x2
+    PreviousOccurrence = 0x3
+
+
+class FileControlInformation(IntEnum):
+    ReturnFCITemplate = 0x00
+    ReturnFCPTempalte = 0x10
+    ReturnFMDTemplate = 0x20
+    NoResponseOrProprietary = 0x30
 #
 # Commands for interchange
 #
+
 
 def GET_RESPONSE(CLA: str, Le: str):
     return CommandApdu.from_dict({CommandField.Class: CLA, CommandField.Instruction: 'C0', CommandField.P1: '00', CommandField.P2: '00', CommandField.Le: Le})
 
 
-def SELECT(CLA: str, P1: str, P2: str, Identifier: str = None, Le: str = None):
-    apdu_dict = {CommandField.Class: CLA, CommandField.Instruction: 'A4',
-                 CommandField.P1: P1, CommandField.P2: P2}
+def SELECT(secure_messaging: SecureMessaging, chaining: Chaining, logical_channel: int, selection: Selection, file_occurrence: FileOccurance, fci: FileControlInformation, data: str = None, Le: str = None) -> CommandApdu:
+    apdu_dict = {CommandField.Class: CLA(secure_messaging, chaining, logical_channel).str(),
+                 CommandField.Instruction: 'A4'}
 
-    if Identifier is not None:
-        apdu_dict[CommandField.Data] = Identifier
+    apdu_dict[CommandField.P1] = selection
+    apdu_dict[CommandField.P2] = file_occurrence + fci
+
+    if data is not None:
+        apdu_dict[CommandField.Data] = _clean(data, 'SELECT()', 'data')
 
     if Le is not None:
-        apdu_dict[CommandField.Le] = Le
+        apdu_dict[CommandField.Le] = _clean(Le, 'SELECT()', 'Le')
 
     return CommandApdu.from_dict(apdu_dict)
