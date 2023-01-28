4d464b360e213d6c996fe0ff766cf0ba6827000b
Sebastien Pochic <spochic@ellipse.la>
2023-01-27 21:44:07 -0800

    globalplatform: added convenience imports

--- src/globalplatform/__init__.py
+++ src/globalplatform/__init__.py
@@ -1,2 +1,2 @@
-from globalplatform.encodings import SecureMessaging, CLA
+from globalplatform.encodings import CardPersonalizationLifeCycleData, SecureMessaging, CLA, CPLC
 from globalplatform.commands import GetDataObject, FileOccurrence, ApplicationIdentifier, GET_DATA, SELECT
