fd276f209beffb770f69d3514f066d90da6db9d1
Sebastien Pochic <spochic@ellipse.la>
2023-02-05 21:53:09 -0800

    pcsc.ml: switch to using pcsc.card module

--- src/pcsc/ml.py
+++ src/pcsc/ml.py
@@ -7,17 +7,17 @@ from typing import Union
 # Third party imports
 
 # Local application imports
-from common.ber import parse_to_dict
-from multos import GET_MULTOS_DATA as _GET_MULTOS_DATA, MultosData, MultosDataField
-from pcsc.scard import Protocol, send_apdu
+from multos import GET_MULTOS_DATA as _GET_MULTOS_DATA, MultosData
+from pcsc.card import Protocol, send_apdu
 
 
 def GET_MULTOS_DATA(hcard, protocol: Protocol) -> MultosData:
-    r = send_apdu(hcard, protocol, _GET_MULTOS_DATA())
+    r, error = send_apdu(hcard, protocol, _GET_MULTOS_DATA())
 
-    if r.SW12() == '9000':
-        multos_data = MultosData(r.data())
+    if error is not None:
+        return None, error
+    elif r.SW12() != '9000':
+        return None, F"Response bytes to GET MULTOS DATA is not 9000 (SW12 = {r.SW12()})"
     else:
-        multos_data = None
-
-    return multos_data
+        multos_data = MultosData(r.data())
+        return multos_data, None
