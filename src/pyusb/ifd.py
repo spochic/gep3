cbc83dd67dc3acba149ba9f4d7c88ee9860ad6ec
Sebastien Pochic <spochic@ellipse.la>
2023-05-17 16:18:51 -0700

    pyusb.ifd: make cfg & intf index public fields

--- src/pyusb/ifd.py
+++ src/pyusb/ifd.py
@@ -14,12 +14,12 @@ from iso7816 import CommandApdu, ResponseApdu
 
 # Class definitions
 class InterfaceDevice:
-    def __init__(self, device: usb.core.Device, interface: usb.core.Interface, cfg_index: int, interface_index: int):
+    def __init__(self, device: usb.core.Device, interface: usb.core.Interface, cfg_index: int, interface_index: int, slot: int = 0):
         self.device: usb.core.Device = device
-        self._cfg_index = cfg_index
-        self._interface_index = interface_index
+        self.cfg_index = cfg_index
+        self.interface_index = interface_index
         self.bSeq: int = 0
-        self.bSlot: int = 0
+        self.bSlot: int = slot
 
         for endpoint in interface:
             endpoint_type = usb.util.endpoint_type(endpoint.bmAttributes)
@@ -45,29 +45,29 @@ class InterfaceDevice:
             self.name = F"{self.device.manufacturer} {self.device.product}"
 
         self.serial_number = self.device.serial_number
-        self.identifier = F"{self.serial_number}.{self._cfg_index}.{self._interface_index}"
+        self.identifier = F"{self.serial_number}.{self.cfg_index}.{self.interface_index}"
 
         # Checking whether OS driver already attached
         logging.info(F"{self}: Checking whether OS driver is already attached")
         self._reattach = False
-        if self.device.is_kernel_driver_active(self._interface_index):
+        if self.device.is_kernel_driver_active(self.interface_index):
             logging.debug("Detaching kernel driver")
             self._reattach = True
-            self.device.detach_kernel_driver(self._interface_index)
+            self.device.detach_kernel_driver(self.interface_index)
 
     def __del__(self):
         if self._reattach:
             logging.debug("\nReattaching kernel driver")
-            self.device.attach_kernel_driver(self._interface_index)
+            self.device.attach_kernel_driver(self.interface_index)
 
     def cfg(self) -> usb.core.Configuration:
-        return self.device[self._cfg_index]
+        return self.device[self.cfg_index]
 
     def intf(self, alternate_setting: int = 0) -> usb.core.Interface:
-        return self.cfg()[(self._interface_index, alternate_setting)]
+        return self.cfg()[(self.interface_index, alternate_setting)]
 
     def write_bulk_message(self, message: BulkOutMessage, timeout=None):
-        return self.device.write(self._ep_bulk_out[0], message.array(), timeout)
+        return self.device.write(self._ep_bulk_out[0], message.array, timeout)
 
     def read_bulk_message(self, timeout=None) -> BulkInMessage:
         enpoint_packet_size = self._ep_bulk_in[1]
@@ -88,25 +88,25 @@ class InterfaceDevice:
     def send_bulk_message(self, message: BulkOutMessage, timeout=None) -> BulkInMessage:
         logging.info(F"{self.identifier} --> {message}")
         n = self.write_bulk_message(message, timeout)
-        if n != len(message.array()):
+        if n != len(message.array):
             raise AssertionError(
-                F"{n} bytes written, expecting {len(message.array())}")
+                F"{n} bytes written, expecting {len(message.arra)}")
 
-        bSeq_out = message.bSeq()
+        bSeq_out = message.bSeq
         bSeq_in = -1
         while bSeq_in != bSeq_out:
             message_in = self.read_bulk_message(timeout)
             logging.info(F"{self.identifier} <-- {message_in}")
 
             # Getting the sequence number of the incoming message
-            bSeq_in = message_in.bSeq()
+            bSeq_in = message_in.bSeq
 
         self.bSeq = bSeq_in + 1
 
         return message_in
 
     def __str__(self):
-        return F"{self.name} (serial number = {self.serial_number}, configuration number = {self._cfg_index}, interface number = {self._interface_index})"
+        return F"{self.name} (serial number = {self.serial_number}, configuration number = {self.cfg_index}, interface number = {self.interface_index})"
 
 
 # Function defitions
