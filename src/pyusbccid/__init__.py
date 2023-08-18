f668c6e45811e18ff79914d93bee325acf7293a6
Sebastien Pochic <spochic@ellipse.la>
2023-08-17 17:54:27 -0700

    v0.2: move PyScard & PyUSB modules to new package

--- src/pyusbccid/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ifd import InterfaceDevice, list_interface_devices
-from .ccid_exchange import get_slot_status, icc_power_on, get_parameters, icc_power_off, xfr_block, cold_reset
-from .apdu_exchange import send_apdu, transmit_apdu
-from . import emv_exchange as emv, gp_exchange as gp
