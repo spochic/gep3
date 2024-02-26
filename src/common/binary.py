2b5f4036e1018ee686b9ab6d9447895fd2423a02
Sebastien Pochic <spochic@ellipse.la>
2024-02-26 12:40:25 -0800

    common.binary: new BitString class

--- src/common/binary.py
+++ src/common/binary.py
@@ -1,6 +1,7 @@
 """binary.py
 """
 # Standard library imports
+from __future__ import annotations
 from collections import UserString
 import math
 import operator
@@ -13,18 +14,27 @@ from typing import Callable, Self, Iterable
 
 # Helper functions
 __HEX_DIGITS = '0123456789abcdefABCDEF'
+__BIT_DIGITS = '01'
 __WHITESPACE = '_'
 
 
-def clean_hex_char(c: str) -> str:
+def clean_char(c: str, keep: str, ignore: str, message: str) -> str:
     match c:
-        case c if c in __HEX_DIGITS:
+        case c if c in keep:
             return c
-        case c if c in __WHITESPACE:
+        case c if c in ignore:
             return ''
         case _:
             raise TypeError(
-                F"Character '{c}' not allowed in hexadecimal string")
+                F"Character '{c}' not allowed in {message}")
+
+
+def clean_hex_char(c: str) -> str:
+    return clean_char(c, keep=__HEX_DIGITS, ignore=__WHITESPACE, message="hexadecimal string")
+
+
+def clean_bit_char(c: str) -> str:
+    return clean_char(c, keep=__BIT_DIGITS, ignore=__WHITESPACE, message="binary string")
 
 
 def bitwise_operation(left: str, right: str, operator: Callable[[int, int], int]):
@@ -51,8 +61,8 @@ class HexString(UserString):
             raise ValueError(F"{self.data} has an odd number of nibbles")
 
     @property
-    def bit_string(self) -> str:
-        return ''.join([F"{int(nibble, 16):04b}" for nibble in self.data])
+    def bit_string(self) -> BitString:
+        return BitString(''.join([F"{int(nibble, 16):04b}" for nibble in self.data]))
 
     @property
     def bit_length(self) -> int:
@@ -94,3 +104,49 @@ class HexString(UserString):
 
     def __invert__(self):
         return self ^ HexString('FF' * self.byte_length)
+
+
+# 'BitString' class
+class BitString(UserString):
+    def __init__(self, string: str):
+        value: str = ''.join(map(clean_bit_char, string))
+        super().__init__(value.upper())
+
+    @property
+    def bytes(self) -> bytes:
+        return HexString(self.data).bytes
+
+    @property
+    def byte_length(self) -> int:
+        return HexString(self.data).byte_length
+
+    @property
+    def hex_string(self) -> HexString:
+        if len(self) % 4 == 0:
+            return HexString(format(int(self.data, 2), F"0{len(self)//4}X"))
+        else:
+            raise ValueError(
+                F"{self.data} has a number of bit that is not a multiple of 4")
+
+    @property
+    def int_list(self) -> list[int]:
+        return HexString(self.data).int_list
+
+    def blocks(self, bitsize: int) -> Iterable[Self]:
+        nr_blocks = math.ceil(self.byte_length / bitsize)
+        return (self[i*2*bitsize:(i+1)*2*bitsize] for i in range(nr_blocks))
+
+    def join(self, seq: Iterable[Self]):
+        return self + ''.join([bstr.data for bstr in seq])
+
+    def __or__(self, other: Self):
+        return HexString(bitwise_operation(self.data, other.data, operator.__or__))
+
+    def __xor__(self, other: Self):
+        return HexString(bitwise_operation(self.data, other.data, operator.__xor__))
+
+    def __and__(self, other: Self):
+        return HexString(bitwise_operation(self.data, other.data, operator.__and__))
+
+    def __invert__(self):
+        return self ^ BitString('1' * len(self))
