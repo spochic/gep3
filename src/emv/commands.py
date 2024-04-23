deb0ec148f5e77cf43d124fcd1db2fa09d9dcafc
Sebastien Pochic <spochic@ellipse.la>
2024-04-23 13:42:02 -0700

    emv.commands: use ByteString

--- src/emv/commands.py
+++ src/emv/commands.py
@@ -2,19 +2,18 @@
 """
 
 # Standard library imports
-from enum import Enum
-from typing import Union
+from enum import StrEnum
 
 # Third party imports
 
 # Local application imports
-from common.ber import encode
-from common.hstr import clean as _clean, to_intlist as _to_intlist
-from iso7816.apdu import CommandApdu, CommandField
+# from common.ber import encode
+from common.binary import ByteString
+from iso7816.apdu import CommandApdu, CommandField, create_command_APDU
 
 
 # Enum Definitions
-class ApplicationIdentifier(Enum):
+class ApplicationIdentifier(StrEnum):
     Mastercard = 'A0000000041010'
     Visa = 'A0000000031010'
     Maestro = 'A00000000410103060'
@@ -24,54 +23,53 @@ class ApplicationIdentifier(Enum):
     Dummy_test = 'A0000008861010'
 
 
-class GetDataObject(Enum):
+class GetDataObject(StrEnum):
     ATC = '9F36'
     LastOnlineATCRegister = '9F13'
     PINTryCounter = '9F17'
-    LogFormat = '9F4f'
+    LogFormat = '9F4F'
 
 
 # Command APDUs defined by EMV
 class Select(CommandApdu):
-    def __init__(self, aid: Union[str, ApplicationIdentifier]):
-        if isinstance(aid, ApplicationIdentifier):
-            data = _to_intlist(aid.value)
-        elif isinstance(aid, str):
-            data = _to_intlist(aid, "emv.SELECT()", 'aid')
-        else:
-            raise TypeError(
-                F"emv.Select(): aid should be of type str or ApplicationIdentifier, received: {type(aid)}")
-
-        super().__init__(0x00, 0xA4, 0x04, 0x00, data=data, Le=0x100)
+    def __init__(self, aid: ByteString):
+        super().__init__(ByteString(
+            F'00A40400{len(aid):02X}') + aid + ByteString('00'))
 
 
-def SELECT(aid: Union[str, ApplicationIdentifier]) -> Select:
+def SELECT(aid: ByteString | ApplicationIdentifier) -> Select:
     """SELECT(): generate CommandApdu for SELECT command
     """
-    return Select(aid)
+    if isinstance(aid, ApplicationIdentifier):
+        return Select(ByteString(aid.value))
+    elif isinstance(aid, ByteString):
+        return Select(aid)
+    else:
+        raise TypeError(
+            F"emv.SELECT(): aid should be of type ByteString or ApplicationIdentifier, received: {type(aid)}")
 
 
 class GetProcessingOptions(CommandApdu):
-    def __init__(self, pdol: str = ''):
-        super().__init__(0x80, 0xA8, 0x00, 0x00,
-                         data=_to_intlist(encode('83', pdol)), Le=0x100)
+    def __init__(self, pdol: ByteString):
+        super().__init__(ByteString(
+            F'80A80000{len(pdol):02X}') + pdol + ByteString('00'))
 
 
-def GET_PROCESSING_OPTIONS(pdol: str = '') -> GetProcessingOptions:
+def GET_PROCESSING_OPTIONS(pdol: ByteString | None) -> GetProcessingOptions:
     """GET_PROCESSING_OPTIONS(): generate APDU for GET PROCESSING OPTIONS command
     """
-    return GetProcessingOptions(pdol)
+    if pdol:
+        return GetProcessingOptions(pdol)
+    else:
+        return GetProcessingOptions(ByteString('8300'))
 
 
-def GPO(pdol: str) -> GetProcessingOptions:
-    """GPO(): generate APDU for GET PROCESSING OPTIONS command
-    """
-    return GET_PROCESSING_OPTIONS(pdol)
+GPO = GET_PROCESSING_OPTIONS
 
 
 class ReadRecord(CommandApdu):
     def __init__(self, SFI: int, record: int):
-        super().__init__(0x00, 0xB2, record, SFI*8+4, Le=0x100)
+        super().__init__(ByteString(F"00B2{record:02X}{SFI*8+4}00"))
 
 
 def READ_RECORD(SFI: int, record: int) -> ReadRecord:
@@ -81,23 +79,27 @@ def READ_RECORD(SFI: int, record: int) -> ReadRecord:
 
 
 class GetData(CommandApdu):
-    def __init__(self, tag: Union[str, GetDataObject]):
-        if isinstance(tag, GetDataObject):
-            P1 = int(tag.value[0:2], 16)
-            P2 = int(tag.value[2:4], 16)
-
-        elif isinstance(tag, str):
-            P1 = int(tag[0:2], 16)
-            P2 = int(tag[2:4], 16)
-
-        else:
-            raise TypeError(
-                F"emv.GetData(): tag should be of type str or GetDataObject, received: {type(tag)}")
-
-        super().__init__(0x80, 0xCA, P1, P2, Le=0x100)
+    def __init__(self, tag: ByteString):
+        super().__init__(ByteString(F'80CA{tag}00'))
 
 
-def GET_DATA(tag: Union[str, GetDataObject]) -> GetData:
+def GET_DATA(tag: ByteString | GetDataObject) -> GetData:
     """GET_DATA: generate APDU for GET DATA command
     """
-    return GetData(tag)
+    if isinstance(tag, GetDataObject):
+        if len(tag.value) == 2:
+            P1 = ByteString('00')
+            P2 = ByteString(tag.value)
+        else:
+            P1 = ByteString(tag.value[0:2])
+            P2 = ByteString(tag.value[2:4])
+
+    elif isinstance(tag, ByteString):
+        P1 = tag[0:2]
+        P2 = tag[2:4]
+
+    else:
+        raise TypeError(
+            F"emv.GET_DATA(): tag should be of type ByteString or GetDataObject, received: {type(tag)}")
+
+    return GetData(P1+P2)
