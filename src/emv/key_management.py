e57bdabc3210da2b97dcdce15776e64f22ef09e4
Sebastien Pochic <spochic@ellipse.la>
2024-04-23 12:49:28 -0700

    emv.key_management: use ByteString + adjust parity

--- src/emv/key_management.py
+++ src/emv/key_management.py
@@ -4,21 +4,31 @@
 # Standard library imports
 
 # Third party imports
+try:
+    from icecream import ic
+    ic.disable()
+except ImportError:  # Graceful fallback if IceCream isn't installed.
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
 # Local application imports
 from common import hstr
+from common.binary import ByteString
 from crypto import des
 
-def master_key_derivation_A(imk, pan, psn = '00'):
+
+# Function definitions
+def master_key_derivation_A(imk: ByteString, pan: str, psn: str = '00'):
     """master_key_derivation_A(): ICC Master Key derivation (Option A)
     """
     X = pan + psn
     if len(X) < 16:
         X = '0' * (16 - len(X)) + X
 
-    Y = X[-16:]
+    Y = ByteString(X[-16:])
+    ic(X, Y)
 
-    Zl = des.tdea_2_ede(imk, Y)
-    Zr = des.tdea_2_ede(imk, hstr.not_(Y))
+    Zl = des.adjust_parity(des.tdea_2_ede(imk, Y))
+    Zr = des.adjust_parity(des.tdea_2_ede(imk, ~Y))
+    ic(Zl, Zr)
 
     return Zl + Zr
